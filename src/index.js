52f95eb7a6884f65b780d8947e4c7dd3984d6222
Sergey Crew <sergeycrew.bs@gmail.com>
2021-06-28 17:26:13 -0700

    built

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import dishesTemplates from './partials/example.hbs';
+import dishes from './menu.json';
 import './sass/main.scss';
+
+
+const refs = {
+    menu: document.querySelector(".js-menu"),
+    themeSwitch: document.querySelector(".theme-switch__toggle"),
+    body: document.querySelector("body"),
+  };
+  
+  const markup = dishesTemplates(dishes);
+  refs.menu.insertAdjacentHTML('beforeend', markup);
+
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+
+const theme = localStorage.getItem("theme");
+
+refs.body.classList.add(!theme ? Theme.LIGHT : theme);
+
+refs.themeSwitch.checked = theme === "dark-theme" ? true : false;
+
+
+
+refs.themeSwitch.addEventListener("change", switchHendler);
+
+function switchHendler() {
+  refs.body.classList.toggle(Theme.DARK);
+  refs.body.classList.toggle(Theme.LIGHT);
+  localStorage.setItem("theme", refs.body.className);
+}
+console.dir(refs.body)
